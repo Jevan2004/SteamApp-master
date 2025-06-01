b0f22ba0bc37bef6263986321f739229eda08722
Tanasa Stefan-Alexandru <147146093+Jevan2004@users.noreply.github.com>
2025-06-01 19:20:26 +0300

    Deploy

--- services/api.js
+++ services/api.js
@@ -1,6 +1,6 @@
 import { tryOrQueue } from "@/app/utils/offlineSync";
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getGames = async (params = {}) => {
   try {
